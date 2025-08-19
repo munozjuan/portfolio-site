9bf4aa3be446105cdc5b7de59c5d85a70c0a1566
Juan Munoz <67563592+munozjuan@users.noreply.github.com>
2025-08-18 20:53:43 -0400

    vite: base=/ for custom domain

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-export default defineConfig({ base: '/', plugins: [react()] })
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  base: "/",   // custom domain => serve from root
+});
